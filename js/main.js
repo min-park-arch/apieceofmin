f77323078228f91d7afe7dbc21cb212d85a0f257
minpark-creator <0707369@gmail.com>
2021-04-26 16:47:37 +0900

    loading + image file correction

--- js/main.js
+++ js/main.js
@@ -1,13 +1,67 @@
-
-$(document).ready(function() {
-	
-$('.menu , .linee').on('click', function() {
-  $('.menu').toggleClass('over')
-  $('.linea1').toggleClass('overL1')
-  $('.linea2').toggleClass('overL2')
-  $('.linea3').toggleClass('overL3')
-  $('.main-menu').toggleClass('overmain')
-  
-});
-$("html").niceScroll({cursorwidth: '8px',cursorcolor:"#222222" , cursorborder:"1px solid #FFF" , autohidemode: false, zindex: 9999 });
+
+
+$(document).ready(function() {
+
+	
+
+$('.menu , .linee').on('click', function() {
+
+  $('.menu').toggleClass('over')
+
+  $('.linea1').toggleClass('overL1')
+
+  $('.linea2').toggleClass('overL2')
+
+  $('.linea3').toggleClass('overL3')
+
+  $('.main-menu').toggleClass('overmain')
+
+  
+
 });
+
+$("html").niceScroll({cursorwidth: '8px',cursorcolor:"#222222" , cursorborder:"1px solid #FFF" , autohidemode: false, zindex: 9999 });
+
+});
+
+<script>
+function test(imageName) {
+    LoadingWithMask('images/loader.png');
+    setTimeout("closeLoadingWithMask()", 3000);
+}
+ 
+function LoadingWithMask(gif) {
+    //화면의 높이와 너비를 구합니다.
+    var maskHeight = $(document).height();
+    var maskWidth  = window.document.body.clientWidth;
+     
+    //화면에 출력할 마스크를 설정해줍니다.
+    var mask       ="<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+    var loadingImg ='';
+      
+    loadingImg +=" <img src='"+ gif +"' style='position: absolute; display: block; margin: 0px auto;'/>";
+ 
+    //화면에 레이어 추가
+    $('body')
+        .append(mask)
+ 
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+    $('#mask').css({
+            'width' : maskWidth,
+            'height': maskHeight,
+            'opacity' :'0.3'
+    });
+  
+    //마스크 표시
+    $('#mask').show();
+  
+    //로딩중 이미지 표시
+    $('#loadingImg').append(loadingImg);
+    $('#loadingImg').show();
+}
+ 
+function closeLoadingWithMask() {
+    $('#mask, #loadingImg').hide();
+    $('#mask, #loadingImg').empty(); 
+}
+
